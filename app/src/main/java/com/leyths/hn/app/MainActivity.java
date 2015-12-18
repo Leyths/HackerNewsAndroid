c9db065bb933586aee69f11a341684a64401807b
David Simkin <david.simkin@guardian.co.uk>
2015-12-18 21:14:07 +0000

    More basic setup

--- app/src/main/java/com/leyths/hn/app/MainActivity.java
+++ app/src/main/java/com/leyths/hn/app/MainActivity.java
@@ -1,15 +1,35 @@
 package com.leyths.hn.app;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import com.leyths.hn.R;
+import com.leyths.hn.fragments.ListFragment;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.content) protected ViewGroup content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        if(savedInstanceState == null) {
+            setupInitialFragment();
+        }
+    }
+
+    private void setupInitialFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content, ListFragment.newInstance(), ListFragment.FRAGMENT_TAG);
+        ft.commit();
     }
 }
--- app/src/main/java/com/leyths/hn/fragments/ListFragment.java
+++ app/src/main/java/com/leyths/hn/fragments/ListFragment.java
@@ -1,10 +1,40 @@
 package com.leyths.hn.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leyths.hn.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class ListFragment extends Fragment {
 
+    public static final String FRAGMENT_TAG = "listFragment";
+
+    @InjectView(R.id.recyclerview) protected RecyclerView recyclerView;
+
+    public static ListFragment newInstance() {
+        ListFragment listFragment = new ListFragment();
+        Bundle args = new Bundle();
+        listFragment.setArguments(args);
+        return listFragment;
+    }
+
     public ListFragment() {
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 }
